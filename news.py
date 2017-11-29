dc54e04ff68c0b3773f89695364b3021600dacc1
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-11-30 00:13:40 +0530

    fix: news.py corrected

--- news.py
+++ news.py
@@ -2,21 +2,15 @@ import psycopg2
 import query
 
 
-DBNAME = "newsdata"
 def mysol(qr):
-
     #connect with database
-    conn = psycopg2.connect(database=DBNAME)
+    conn = psycopg2.connect("host='localhost' dbname='news' user='postgres' password= 'password'")
     c = conn.cursor()
-
     #QUery execution
     c.execute(qr)
-
     #result
     res = c.fetchall()
-
     print(res)
-
     conn.close()
 
 def main():
@@ -24,6 +18,5 @@ def main():
     mysol(query.mysecondquery)
     mysol(query.mythirdquery)
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     main()
-
