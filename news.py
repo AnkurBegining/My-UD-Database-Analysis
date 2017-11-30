ff828f04892e25dec8f950d4e9cb34630ff6bf5c
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-11-30 10:50:29 +0530

    fix: res formatted

--- news.py
+++ news.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3.6
+
 import psycopg2
 import query
 
 
-def mysol(qr):
+def mysol(qr, str):
     #connect with database
     conn = psycopg2.connect("host='localhost' dbname='news' user='postgres' password= 'password'")
     c = conn.cursor()
@@ -10,13 +12,27 @@ def mysol(qr):
     c.execute(qr)
     #result
     res = c.fetchall()
-    print(res)
+
+    if str == 'first':
+        print("\nWhat are the top three articles of all time?\n")
+        for title, views in res:
+            print("    {}  -  {} views".format(title, views))
+    elif str == 'second':
+        print("\nWho are the most popular article authors of all time?\n")
+        for title, views in res:
+            print("    {}  -  {} views".format(title, views))
+    else:
+        print("\nOn which days did more than 1% of requests lead to errors?\n")
+        for da, err in res:
+            print("    {}  -  {} error".format(da, err))
+
+
     conn.close()
 
 def main():
-    mysol(query.myfirstquery)
-    mysol(query.mysecondquery)
-    mysol(query.mythirdquery)
+    mysol(query.myfirstquery, 'first')
+    mysol(query.mysecondquery, 'second')
+    mysol(query.mythirdquery, 'third')
 
 if __name__ == '__main__':
     main()
